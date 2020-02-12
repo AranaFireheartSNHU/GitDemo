56f72e88adac7810c517602bab1c3fc040bc2cfc
DragonFired <aranafireheart@mac.com>
2020-02-12 13:18:52 -0500

    Added empty fabulousClass.

---
+++ fabulousClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Arana Fireheart on 2/12/20.
+//
+
+#include "fabulousClass.h"
---
+++ fabulousClass.h
@@ -0,0 +1,8 @@
+//
+// Created by Arana Fireheart on 2/12/20.
+//
+
+#ifndef GITDEMO_FABULOUSCLASS_H
+#define GITDEMO_FABULOUSCLASS_H
+
+#endif //GITDEMO_FABULOUSCLASS_H
